400777e31eb44bc8d22bc5548bd242b31b5ec00e
Marcus Novak <marcusanovak@gmail.com>
2014-12-03 15:45:08 -0600

    Add curves to spline, find which curve at certain point.

--- Assets/Editor/BezierSplineInspector.cs
+++ Assets/Editor/BezierSplineInspector.cs
@@ -21,16 +21,34 @@ public class BezierSplineInspector : Editor
         handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
 
         Vector3 p0 = ShowPoint(0);
-        Vector3 p1 = ShowPoint(1);
-        Vector3 p2 = ShowPoint(2);
-        Vector3 p3 = ShowPoint(3);
+        for (int i = 1; i < spline.points.Length; i += 3)
+        {
+            Vector3 p1 = ShowPoint(i);
+            Vector3 p2 = ShowPoint(i + 1);
+            Vector3 p3 = ShowPoint(i + 2);
 
-        Handles.color = Color.gray;
-        Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p2, p3);
+            Handles.color = Color.gray;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p2, p3);
+
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+            p0 = p3;
+        }
 
         ShowDirections();
-        Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        spline = target as BezierSpline;
+        if (GUILayout.Button("Add Curve"))
+        {
+            Undo.RecordObject(spline, "Add Curve");
+            spline.AddCurve();
+            EditorUtility.SetDirty(spline);
+        }
     }
 
 
--- Assets/Scripts/CurvesAndSplines/BezierSpline.cs
+++ Assets/Scripts/CurvesAndSplines/BezierSpline.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
+using System;
 
 public class BezierSpline : MonoBehaviour 
 {
 
 
     public Vector3[] points;
+
+
+    // Returns the total number of curves in the spline
+    public int CurveCount
+    {
+        get
+        {
+            return (points.Length - 1) / 3;
+        }
+    }
+
 
     public void Reset()
     {
@@ -20,12 +32,44 @@ public class BezierSpline : MonoBehaviour
 
     public Vector3 GetPoint(float t)
     {
+        int i;
+
+        // Default to the last curve if t is greater or equal to 1
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = points.Length - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= 1;
+            i *= 3;
+        }
+
         return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
     }
 
 
     public Vector3 GetVelocity(float t)
     {
+        int i;
+
+        // Default to last curve if t is greater or equal to 1
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = points.Length - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+
         return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) - transform.position;
     }
 
@@ -33,6 +77,21 @@ public class BezierSpline : MonoBehaviour
     public Vector3 GetDirection(float t)
     {
         return GetVelocity(t).normalized;
+    }
+
+
+    public void AddCurve()
+    {
+        Vector3 point = points[points.Length - 1];   // First point of new curve is the same as the last point of the previous
+        Array.Resize(ref points, points.Length + 3); // Increase the size of our points array by the amount of new points needed
+
+        // Offset the x value of each new point by 1 so they don't sit on top of each other
+        point.x += 1f;
+        points[points.Length - 3] = point;
+        point.x += 1f;
+        points[points.Length - 2] = point;
+        point.x += 1f;
+        points[points.Length - 1] = point;
     }
 
 
